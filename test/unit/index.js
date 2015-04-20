0414ba7f208ed6f8884cd3afe25a5dd9fa6a416f
Scott Lowe <scott@entercastle.com>
2015-04-20 12:13:31 -0400

    test(StorageService): add test suite

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,9 +1,22 @@
 var StorageService = require('../../src/index.js');
+var RecordService = require('../../src/record-service.js');
+var CollectionService = require('../../src/collection-service.js');
 var expect = require('chai').expect;
 
 describe('StorageService', function() {
-  it('should respect logic', function() {
-    expect(true).to.equal(true);
-    expect(true).to.not.equal(false);
+  var mockStorageProvider = {};
+  it('should require a provider', function() {
+    expect(function () { var storage = new StorageService(); }).to.throw();
+  });
+  it('should create records', function() {
+    var storage = new StorageService(mockStorageProvider);
+    var record = storage.createRecord('Cat');
+    expect(record).to.be.an.instanceof(RecordService);
+    expect(record.getType()).to.equal('Cat');
+  });
+  it('should create collections', function() {
+    var storage = new StorageService(mockStorageProvider);
+    var collection = storage.createCollection('Cat');
+    expect(collection).to.be.an.instanceof(CollectionService);
   });
 });
